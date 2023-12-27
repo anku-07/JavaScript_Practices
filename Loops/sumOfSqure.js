26837802c028f8dfd57a82a4ad1498f3a5f8dad8
anku-07 <adityamondal48765@gmail.com>
2023-12-27 12:10:25 +0530

    Added

---
+++ Loops/sumOfSqure.js
@@ -0,0 +1,8 @@
+/*
+ find the sum of squares of numbers from 1 to 5 using a loop in 
+ */
+let sumOfSquare = 0;
+for(let i = 1; i<=5; i++){
+    sumOfSquare += i * i;
+}
+console.log(`the sum of square from 1 to 5 is ${sumOfSquare}`);
