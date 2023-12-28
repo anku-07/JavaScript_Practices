3708e246b51993f80830653a28ba44b2c455c6a1
anku-07 <adityamondal48765@gmail.com>
2023-12-28 21:33:34 +0530

    Added new file

---
+++ While Loop/sumOfNumber.js
@@ -0,0 +1,9 @@
+// Develop a program that calculates the sum of numbers from 1 to 50 using a while loop.
+let sum = 0;
+let i =1;
+let num = 50;
+while(i <= num){
+    sum = sum + i;
+    i++;
+}
+console.log("Sum of 1 to 50 is ",sum);
