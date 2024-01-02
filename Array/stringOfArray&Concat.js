02e8bbd4fe906a7a5fdeda24c87408da43c09540
anku-07 <adityamondal48765@gmail.com>
2024-01-02 22:39:40 +0530

    Added string of array and concat together sting

---
+++ Array/stringOfArray&Concat.js
@@ -0,0 +1,12 @@
+/*
+Write a function that takes an array of strings and returns the strings concatenated together.
+*/
+function concatenateStrings(arr) {
+    return arr.join('');
+  }
+  
+  // Example usage:
+  const strings = ['Hello', ' ', 'World', '!'];
+  const concatenatedString = concatenateStrings(strings);
+  console.log(concatenatedString); // Output will be 'Hello World!'
+  
