2ccf4db80d44992d4cf506d5372d3ffebec2bf19
anku-07 <adityamondal48765@gmail.com>
2024-01-04 09:51:07 +0530

    Print table using function

---
+++ Function/calculateAvg.js
@@ -0,0 +1,8 @@
+// Create a function that calculate us the avarage of 3 numbers.
+function calculateAvg(a,b,c){
+    let avg = (a+b+c)/3;
+    console.log(avg);
+}
+
+// Call the function
+calculateAvg(4,5,9);
---
+++ Function/numberTable.js
@@ -0,0 +1,7 @@
+// Create a function that prints the multiplication table of a number.
+function printTable(n){
+    for(let i = 1 ; i <= 10 ; i++){
+        console.log(n*i);
+    }
+}
+printTable(2);
