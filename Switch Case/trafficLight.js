625ad74b8769a1ee17c68918df744a43bb5a0bdb
anku-07 <adityamondal48765@gmail.com>
2024-01-05 12:07:35 +0530

    traffic light using switch case

---
+++ Switch Case/trafficLight.js
@@ -0,0 +1,10 @@
+// Traffic light using Switch case.
+let color = 'green';
+
+switch (color){
+    case 'red': console.log("Stop!");
+    break;
+    case 'green' : console.log("go");
+    break;
+    default : console.log("Broken Light");
+}
