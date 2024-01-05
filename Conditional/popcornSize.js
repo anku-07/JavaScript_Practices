a0d6e2892be9a8aab2e37dbd0f1f6b28c87ef373
anku-07 <adityamondal48765@gmail.com>
2024-01-05 11:17:16 +0530

    Popcorn size related price

---
+++ Conditional/popcornSize.js
@@ -0,0 +1,17 @@
+// Create a system to calculate popcorn price based on the size customer asked for :
+/*
+if size is 'xl',price is rs = 250;
+if size is 'l',price is rs = 200;
+if size is 'm', price is rs = 100;
+if size is 's', price is rs = 50;
+*/
+let size = 'xl';
+if(size == 'xl'){
+    console.log("Price is : 250");
+}else if(size == 'l'){
+    console.log("Price is : 200");
+}else if(size == 'm'){
+    console.log("Price is : 100");
+} else if(size == 's'){
+    console.log("Price is : 50");
+}
