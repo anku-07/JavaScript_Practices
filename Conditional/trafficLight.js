f705361ce7d90ee9a1134c59a78fb46928c8f9e8
anku-07 <adityamondal48765@gmail.com>
2024-01-05 11:10:11 +0530

    Traffic light color using if else

---
+++ Conditional/trafficLight.js
@@ -0,0 +1,12 @@
+//create a traffic light system that shows what to do based color.
+let color = "blue";
+
+if(color == "green"){
+    console.log("GO");
+}else if(color == "yellow"){
+    console.log("wait");
+}else if(color == "red"){
+    console.log("Stop");
+}else{
+    console.log("Enter valid traffic light color.");
+}
