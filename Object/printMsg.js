ea3bff1eac1fdae8fe89b55b37149aae2cc23dc3
anku-07 <adityamondal48765@gmail.com>
2024-01-05 12:21:58 +0530

    Print Msg

---
+++ Object/printMsg.js
@@ -0,0 +1,4 @@
+// Take the user's name & age as input prompts then return back the following statement to the user as an alert.
+let name = prompt("Enter your name : ");
+let age = prompt("Enter your age : ");
+console.log(`My name is ${name} and age is ${age}.`);
