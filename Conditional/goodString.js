706ab49836d58b82651893c86d72740b55fc61fd
anku-07 <adityamondal48765@gmail.com>
2024-01-05 11:26:48 +0530

    Check good string or bad string

---
+++ Conditional/goodString.js
@@ -0,0 +1,7 @@
+// a "Good string" that starts with the letter 'a' and has length > 3 write a program to find if s string is good or not.
+let str = "aditya";
+if(str[0] == 'a' && str.length > 3){
+    console.log(`It is a good string.`);
+}else{
+    console.log("It is a bad string.");
+}
