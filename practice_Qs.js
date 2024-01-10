ee3fcf75872858d7c7ec03e8c334f57feff78e5a
anku-07 <adityamondal48765@gmail.com>
2024-01-10 23:33:14 +0530

    Added new file

---
+++ practice_Qs.js
@@ -0,0 +1,13 @@
+// Qn- 1 For the crive string
+
+let msg = '           help                   ';
+msg = msg.trim().toUpperCase();
+console.log(msg);
+ // Qn-2
+let tutor = 'Apna College';
+console.log(tutor.slice(4,9));
+console.log(tutor.indexOf('na'));
+console.log(tutor.replace('Apna','Our'));
+
+//Qn-3
+console.log(tutor.replace('l','t').replace('l','t'));
