78df515852bc37a6b17cdf9cdfcf57bfd196a360
anku-07 <adityamondal48765@gmail.com>
2024-01-10 23:44:50 +0530

    Added swaping two number without using third veriabl

---
+++ swapTwoNumberWithoutThird.js
@@ -0,0 +1,8 @@
+//Without third veriable
+let a = 10;
+let b = 20;
+console.log(`Before swaping a = ${a} & b = ${b}.`);
+a = a+b;
+b = a-b;
+a = a-b;
+console.log(`Before swaping a = ${a} & b = ${b}.`);
