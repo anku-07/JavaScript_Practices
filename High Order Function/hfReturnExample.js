0237d9974f18fdf51141b0d915f766018d2f9367
anku-07 <adityamondal48765@gmail.com>
2024-01-10 19:13:06 +0530

    Added higher order function with returns

---
+++ High Order Function/hfReturnExample.js
@@ -0,0 +1,25 @@
+// Return a function.
+let odd = function(n){
+    console.log(!(n%2 == 0));
+}
+let even = function(n){
+    console.log((n%2 == 0));
+}
+
+function oddEventest(request){
+    if(request == 'odd'){
+        let odd = function(n){
+            console.log(!(n%2 == 0));
+        }
+        return odd;
+    }else if(request == 'even'){
+        let even = function(n){
+            console.log((n%2 == 0));
+        }
+        return even;
+    }else{
+        console.log("Wrong request.")
+    }
+}
+
+let request = "odd" // Even
