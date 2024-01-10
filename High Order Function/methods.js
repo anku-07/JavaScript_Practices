c5547554bf6bcf9f751866a6dbd61301bcacd54f
anku-07 <adityamondal48765@gmail.com>
2024-01-10 19:32:51 +0530

    Added function methods file

---
+++ High Order Function/methods.js
@@ -0,0 +1,27 @@
+// Function in object.
+
+// const calculetor = {
+//     add : function(a,b){
+//         return a + b;
+//     },
+//     sub : function(a,b){
+//         return a - b;
+//     },
+//     mul : function(a,b){
+//         return a * b;
+//     }
+// };
+
+// Shorthand
+
+const calculetor = {
+    add(a,b){
+        return a + b;
+    },
+    sub(a,b){
+        return a - b;
+    },
+    mul(a,b){
+        return a * b;
+    }
+};
