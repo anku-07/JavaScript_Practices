07f787d75c1ead944090c79cf91f4b8f0efdddf4
anku-07 <adityamondal48765@gmail.com>
2024-01-10 23:39:35 +0530

    Added swaping two number using third veriable

---
+++ swapTwoNumber.js
@@ -0,0 +1,11 @@
+// Write a program in c to print swap two numbers.
+let a = 10;
+let b = 20;
+let temp;
+console.log(`Before swaping a = ${a} & b = ${b}.`);
+
+temp = a;
+a = b;
+b = temp;
+console.log(`After swaping a = ${a} & b = ${b}.`);
+
