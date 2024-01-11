be93b5ec0a61e711a76732ce7a82eb59e680c507
anku-07 <adityamondal48765@gmail.com>
2024-01-11 11:58:26 +0530

    Try and catch

---
+++ tryCatch.js
@@ -0,0 +1,20 @@
+// try&catch
+console.log("Hello");
+console.log("Hello");
+
+// for testing error we used try and catch
+// try{
+//     console.log(a);
+// }catch{
+//     console.log("Catch an error... a is not defined");
+// }
+
+// we can do this
+try{
+    console.log(a);
+}catch(err){
+    console.log("caught an error a is not defined")
+    console.log(err);
+}
+console.log("Hello2");
+console.log("Hello2");
