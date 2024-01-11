0a1accda12cbfde346c850f004ec427712d891cf
anku-07 <adityamondal48765@gmail.com>
2024-01-11 12:54:42 +0530

    Added arrow funtion definition rules

---
+++ ArrowFunction/arrowFunction.js
@@ -0,0 +1,12 @@
+//Sum of two number
+const sum = (a,b) => {
+    console.log(a+b);
+};
+// cube 
+const cube = (n) => { // we can pass an argument without perethiases for example cube = n => {definition}
+    return n*n*n;
+}
+//print Hello
+const hello = () => { // when no one argument has not exist in an function so we must use perethiases for example hello =  => is an in valid syntax.
+    console.log("Hello world!");
+}
