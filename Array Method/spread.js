ebe2e4937c83e5c77d0c4b992e3e4c15e6ac8150
anku-07 <adityamondal48765@gmail.com>
2024-01-14 18:49:14 +0530

    spread

---
+++ Array Method/spread.js
@@ -0,0 +1,5 @@
+let arr = [3,4,5,6,7,8,9,1,0];
+console.log(Math.min(...arr)); // for minimum value in array eliment
+console.log(Math.max(...arr)); // for printing maximum value in array eliment
+console.log(...arr); // for printing each eliment in array
+console.log(..."Aditya"); // for printing string indivsual character
