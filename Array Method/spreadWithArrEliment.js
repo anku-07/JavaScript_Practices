d6b63ccf742adacf14bdfd31c110616bdac5f6fc
anku-07 <adityamondal48765@gmail.com>
2024-01-14 18:53:40 +0530

    spread with array eliment

---
+++ Array Method/spreadWithArrEliment.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4,5];
+let newArr = [...arr]; // for copy arr eliment in an new Array
+console.log(newArr); // output : [1,2,3,4,5];
+
+let chars = [..."hello"]; // output : ['h', 'e', 'l', 'l', 'o'];
