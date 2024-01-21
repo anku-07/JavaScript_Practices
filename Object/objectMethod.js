fe47204631f14c194e7e9eeffe7b6e22465d034d
anku-07 <adityamondal48765@gmail.com>
2024-01-21 19:18:42 +0530

    Object method add

---
+++ Object/objectMethod.js
@@ -0,0 +1,16 @@
+//Create an object called calculator with methods for addition, subtraction, multiplication, and division. Each method should take two parameters and perform the respective operation. Test each method with appropriate arguments and log the results.
+
+const calculetor = {
+    add : function(a,b){
+        return a + b;
+    },
+    sub(a,b){
+        return a - b;
+    },
+    mul(a,b){
+        return a * b;
+    },
+    div(a,b){
+        return a / b;
+    }
+}
