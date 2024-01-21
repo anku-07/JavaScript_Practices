8296a9f04175a618d565144bcd24246d83be1896
anku-07 <adityamondal48765@gmail.com>
2024-01-21 19:38:33 +0530

    object method with the parameter

---
+++ Object/objectMethodWithParameter.js
@@ -0,0 +1,7 @@
+//Create an object called person with a method greet that takes a name parameter and logs a greeting message. Test the method with different names.
+
+const person = {
+    greet(name){
+        console.log("Hello " + name + "!");
+    }
+}
